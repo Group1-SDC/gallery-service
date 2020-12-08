4ca8683c2447bb7f8206dc976e2a9436c637e703
Luke Hatcher <lukehatcher98@gmail.com>
2020-12-08 15:07:31 -0800

    setup db connection and export query fxn

--- database/index.js
+++ database/index.js
@@ -1,2 +1,25 @@
-// set connection
-// postgreSQL query handler functions here
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'lukehatcher',
+  host: 'localhost',
+  database: 'gallery',
+  password: '',
+  port: 5432,
+});
+
+pool.connect();
+
+const query = (queryString, cb) => {
+  pool.query(queryString)
+    .then((res) => {
+      cb(res.rows);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
+
+module.exports = {
+  query,
+};
